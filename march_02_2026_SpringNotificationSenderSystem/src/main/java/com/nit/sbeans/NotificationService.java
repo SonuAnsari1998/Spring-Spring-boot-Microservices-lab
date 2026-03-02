21c10dda77e40ca9b9dea38190147c6f2c93eff5
Sonu Ansari <786sonuansari123@gmail.com>
2026-03-02 16:43:49 +0530

    Notification sender spring

--- march_02_2026_SpringNotificationSenderSystem/src/main/java/com/nit/sbeans/NotificationService.java
+++ march_02_2026_SpringNotificationSenderSystem/src/main/java/com/nit/sbeans/NotificationService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificationService {
 	@Autowired
-	@Qualifier("emailService")
+	@Qualifier("whatsAppService")
 	MessageService messageService;
 
 	public void notifyUser(String msg) {
